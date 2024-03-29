6ac830bf8f00d65ca54c8ecfbe0099251195b945
misaki-kawaguchi <m.kawaguchi1122@gmail.com>
2024-03-29 09:50:34 +0900

    first commit

---
+++ LOTRConverter17/LOTRConverter17App.swift
@@ -0,0 +1,17 @@
+//
+//  LOTRConverter17App.swift
+//  LOTRConverter17
+//
+//  Created by 川口美咲 on 2024/03/29.
+//
+
+import SwiftUI
+
+@main
+struct LOTRConverter17App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
