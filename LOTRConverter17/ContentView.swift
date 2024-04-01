c5aa3053ca06d51a3e100eaece952ef24098f14b
misaki-kawaguchi <m.kawaguchi1122@gmail.com>
2024-04-01 10:12:01 +0900

    背景画像の上にテキスト・画像を追加

--- LOTRConverter17/ContentView.swift
+++ LOTRConverter17/ContentView.swift
@@ -17,8 +17,15 @@ struct ContentView: View {
             
             VStack {
                 // Prancing pony image
+                Image(.prancingpony)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 200)
                 
                 // Currency exchange text
+                Text("Currency Exchange")
+                    .font(.largeTitle)
+                    .foregroundStyle(.white)
                 
                 // Currency conversion section
                 HStack {
@@ -27,29 +34,55 @@ struct ContentView: View {
                         // Currency
                         HStack {
                             // Currency image
+                            Image(.silverpiece)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: 33)
                             
                             // Currency text
+                            Text("Silver Piece")
+                                .font(.headline)
+                                .foregroundStyle(.white)
                         }
                         
                         // Textfield
+                        Text("Textfield")
                     }
                     
                     // Equal sign
-                    
+                    Image(systemName: "equal")
+                        .font(.largeTitle)
+                        .foregroundStyle(.white)
+                        .symbolEffect(.pulse)
                     // Right conversion section
                     VStack {
                         // Currency
                         HStack {
                             // Currency text
+                            Text("Gold Piece")
+                                .font(.headline)
+                                .foregroundStyle(.white)
                             
                             // Currency image
+                            Image(.goldpiece)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: 33)
                         }
                         
                         // Textfield
+                        Text("Textfield")
                     }
                 }
                 
+                Spacer()
+                
+                // Info button
+                Image(systemName: "info.circle.fill")
+                    .font(.largeTitle)
+                    .foregroundStyle(.white)
             }
+//            .border(.blue)
         }
     }
 }
