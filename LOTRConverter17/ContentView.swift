3daca546d54a5e800a81a57db97a8e70d08c2134
misaki-kawaguchi <m.kawaguchi1122@gmail.com>
2024-04-01 10:39:05 +0900

    Info Buttonを作成・showExchangeInfoを変更できるようにする

--- LOTRConverter17/ContentView.swift
+++ LOTRConverter17/ContentView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var showExchangeInfo = false
+    
     var body: some View {
         ZStack {
             // Background image
@@ -78,11 +80,19 @@ struct ContentView: View {
                 Spacer()
                 
                 // Info button
-                Image(systemName: "info.circle.fill")
-                    .font(.largeTitle)
-                    .foregroundStyle(.white)
+                HStack {
+                    Spacer()
+                    Button {
+                        showExchangeInfo.toggle()
+                    } label: {
+                        Image(systemName: "info.circle.fill")
+                            .font(.largeTitle)
+                            .foregroundStyle(.white)
+                    }
+                    .padding(.trailing)
+                }
             }
-//            .border(.blue)
+            //            .border(.blue)
         }
     }
 }
