d739b3dffdf26eb71821f2f2829013af5cb1955d
misaki-kawaguchi <m.kawaguchi1122@gmail.com>
2024-04-01 09:57:18 +0900

    背景画像を表示

--- LOTRConverter17/ContentView.swift
+++ LOTRConverter17/ContentView.swift
@@ -11,6 +11,9 @@ struct ContentView: View {
     var body: some View {
         ZStack {
             // Background image
+            Image(.background)
+                .resizable()
+                .ignoresSafeArea()
             
             VStack {
                 // Prancing pony image
