f5788fa3a33614fa2cd366ce43f2be0504f65bfe
misaki kawaguchi <60394359+misaki-kawaguchi@users.noreply.github.com>
2024-04-03 10:50:17 +0900

    Currencyを作成 (#12)

---
+++ LOTRConverter17/Currency.swift
@@ -0,0 +1,14 @@
+//
+//  Currency.swift
+//  LOTRConverter17
+//
+//  Created by 川口美咲 on 2024/04/03.
+//
+
+enum Currency: Double {
+    case copperPenny = 6400
+    case silperPenny = 64
+    case silverPiece = 16
+    case goldPenny = 4
+    case goldPiece = 1
+}
