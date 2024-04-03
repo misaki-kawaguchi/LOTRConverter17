3762dc5e4b7f5203b9e2b77fbc8294923de5fa60
misaki-kawaguchi <m.kawaguchi1122@gmail.com>
2024-04-03 09:40:04 +0900

    SelectCurrency画面・CurrencyIconコンポーネントを作成

---
+++ LOTRConverter17/SelectCurrency.swift
@@ -0,0 +1,57 @@
+//
+//  SelectCurrency.swift
+//  LOTRConverter17
+//
+//  Created by 川口美咲 on 2024/04/03.
+//
+
+import SwiftUI
+
+struct SelectCurrency: View {
+    @Environment(\.dismiss) var dismiss
+    
+    var body: some View {
+        ZStack {
+            // parchment background image
+            Image(.parchment)
+                .resizable()
+                .ignoresSafeArea()
+                .background(.brown)
+            
+            VStack {
+                // Text
+                Text("Select the currency you are starting with:")
+                    .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                
+                // Currency icons
+            
+                
+                // Text
+                Text("Select the currency you would like to convert to:")
+                    .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                
+                // Currency icons
+                CurrencyIcon(
+                    currencyImage: .copperpenny,
+                    currencyName: "Copper Penny"
+                )
+                
+                // Done button
+                Button("Done") {
+                    dismiss()
+                }
+                .buttonStyle(.borderedProminent)
+                .tint(.brown)
+                .font(.largeTitle)
+                .padding()
+                .foregroundStyle(.white)
+            }
+            .padding()
+            .multilineTextAlignment(.center)
+        }
+    }
+}
+
+#Preview {
+    SelectCurrency()
+}
