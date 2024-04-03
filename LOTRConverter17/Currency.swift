bfce30a38a3f6d84959ded2d0b46902c5f52a55e
misaki kawaguchi <60394359+misaki-kawaguchi@users.noreply.github.com>
2024-04-04 08:43:30 +0900

    Currencyにimageプロパティとnameプロパティを追加 (#13)

--- LOTRConverter17/Currency.swift
+++ LOTRConverter17/Currency.swift
@@ -5,10 +5,42 @@
 //  Created by 川口美咲 on 2024/04/03.
 //
 
+import SwiftUI
+
 enum Currency: Double {
     case copperPenny = 6400
-    case silperPenny = 64
+    case silverPenny = 64
     case silverPiece = 16
     case goldPenny = 4
     case goldPiece = 1
+    
+    var image: ImageResource {
+        switch self {
+        case .copperPenny:
+                .copperpenny
+        case .silverPenny:
+                .silverpenny
+        case .silverPiece:
+                .silverpiece
+        case .goldPenny:
+                .goldpenny
+        case .goldPiece:
+                .goldpiece
+        }
+    }
+    
+    var name: String {
+        switch self {
+        case .copperPenny:
+            "Copper Penny"
+        case .silverPenny:
+            "Silver Penny"
+        case .silverPiece:
+            "Silver Piece"
+        case .goldPenny:
+            "Gold Penny"
+        case .goldPiece:
+            "Gold Piece"
+        }
+    }
 }
