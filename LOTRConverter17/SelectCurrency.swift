08b9922795b415741699fe366e9ac427600b2522
misaki-kawaguchi <m.kawaguchi1122@gmail.com>
2024-04-04 10:09:36 +0900

    IconGridコンポーネントを作成

--- LOTRConverter17/SelectCurrency.swift
+++ LOTRConverter17/SelectCurrency.swift
@@ -9,7 +9,6 @@ import SwiftUI
 
 struct SelectCurrency: View {
     @Environment(\.dismiss) var dismiss
-    @State var currency: Currency
     
     var body: some View {
         ZStack {
@@ -25,34 +24,14 @@ struct SelectCurrency: View {
                     .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
                 
                 // Currency icons
-                LazyVGrid(columns: [GridItem(), GridItem(), GridItem()]) {
-                    ForEach(Currency.allCases) { currency in
-                        if self.currency == currency {
-                            CurrencyIcon(currencyImage: currency.image, currencyName: currency.name)
-                                .shadow(color: /*@START_MENU_TOKEN@*/.black/*@END_MENU_TOKEN@*/, radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
-                                .overlay {
-                                    RoundedRectangle(cornerRadius: 25)
-                                        .stroke(lineWidth: 3)
-                                        .opacity(0.5)
-                                }
-                        } else {
-                            CurrencyIcon(
-                                currencyImage: currency.image,
-                                currencyName: currency.name
-                            )
-                            .onTapGesture {
-                                self.currency = currency
-                            }
-                        }
-                        
-                    }
-                }
+                IconGrid(currency: .silverPiece)
                 
                 // Text
                 Text("Select the currency you would like to convert to:")
                     .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
                 
                 // Currency icons
+                IconGrid(currency: .goldPiece)
                 
                 // Done button
                 Button("Done") {
@@ -71,5 +50,5 @@ struct SelectCurrency: View {
 }
 
 #Preview {
-    SelectCurrency(currency: .silverPiece)
+    SelectCurrency()
 }
