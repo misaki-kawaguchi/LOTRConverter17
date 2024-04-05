c1c54d87e70c2700e7d8d0b464070f0876657be0
misaki kawaguchi <60394359+misaki-kawaguchi@users.noreply.github.com>
2024-04-05 22:54:53 +0900

    @Stateから@Bindingに変更して、選択したCurrencyをContentViewに反映する (#17)

--- LOTRConverter17/ContentView.swift
+++ LOTRConverter17/ContentView.swift
@@ -120,8 +120,8 @@ struct ContentView: View {
         }
         .sheet(isPresented: $showSelectCurrency) {
             SelectCurrency(
-                topCurrency: leftCurrency,
-                bottomCurrency: rightCurrency
+                topCurrency: $leftCurrency,
+                bottomCurrency: $rightCurrency
             )
         }
     }
